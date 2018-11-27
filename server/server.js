b2ad3d1db9ff40a08d8498be54c15da5ed279f63
saswat-pandey <saswat1701@gmail.com>
2018-11-27 08:02:17 -0600

    Added emit functionality

--- server/server.js
+++ server/server.js
@@ -12,6 +12,17 @@ app.use(express.static(publicpath));
 
 io.on("connection",(socket)=>{
   console.log("New user Connected");
+
+socket.emit("NewMsg",{
+  from:"Saswat",
+  text:"This is it",
+  createdAt:1233248763
+});
+
+socket.on("createMsg",(data)=>{
+  console.log(data);
+});
+
   socket.on("disconnect",()=>{
     console.log("Disconnnected with the client");
   });
